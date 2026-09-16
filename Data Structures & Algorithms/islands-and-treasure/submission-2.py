d68a5fada267fe385a6b23479f2bd94f0586a8ee
Naman Jain <54635559+njain2208@users.noreply.github.com>
2026-09-16 20:20:53 +0530

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        infVal = 2**31 - 1
+
+        queue = collections.deque()
+        visit = set()
+        level = 1
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    queue.append((i,j))
+                    visit.add((i,j))
+
+        while queue:
+            for _ in range(len(queue)):
+                a, b = queue.popleft()
+                for x,y in [(a+1,b),(a,b+1),(a-1,b),(a,b-1)]:
+                    if (x<0 or x == len(grid) or y < 0 or y == len(grid[0]) or (x,y) in visit
+                       or grid[x][y] == -1):
+                        continue
+
+                    grid[x][y] = level
+                    queue.append((x,y))
+                    visit.add((x,y))
+            
+            level += 1
+
+
