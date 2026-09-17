0d736e0dcf3e27ba60023813f9d93888f76d3b63
Naman Jain <54635559+njain2208@users.noreply.github.com>
2026-09-17 20:29:21 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        preReqCourses = collections.defaultdict(list)
+
+        visited, cycleNode = set(), set()
+        
+
+        for [a,b] in prerequisites:
+            preReqCourses[a].append(b)
+        
+        def dfs(node):
+            nonlocal visited, cycleNode
+
+            if node in cycleNode:
+                return False
+
+            if node in visited:
+                return True
+            else:
+                visited.add(node)
+            
+            cycleNode.add(node)
+
+            for neigh in preReqCourses[node]:
+                if not dfs(neigh):
+                    return False
+
+            cycleNode.remove(node)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+
+
+        return True
